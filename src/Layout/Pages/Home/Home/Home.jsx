65da545a2da9473f7bf2b73b8fec6dca15bff052
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-06-10 16:16:03 +0600

    Implementing a dark/light theme toggle for the home page done

--- src/Layout/Pages/Home/Home/Home.jsx
+++ src/Layout/Pages/Home/Home/Home.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import Banner from "../Banner/Banner";
 import Classes from "../Classes/Classes";
 import Facilities from "../Facilities/Facilities";
 import Instructors from "../Instructors/Instructors";
 
-
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Classes></Classes>
-            <Instructors></Instructors>
-            <Facilities></Facilities>
-        </div>
-    );
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <div className={` ${isDarkMode ? "dark" : ""}`}>
+      <input
+        type="checkbox"
+        className="toggle toggle-lg absolute top-8 left-1/2"
+        checked={isDarkMode}
+        onChange={toggleTheme}
+      />
+      <div className={`transition-colors duration-500 ${isDarkMode ? "bg-gray-900 text-white" : "bg-white text-black"}`}>
+        <Banner></Banner>
+        <Classes></Classes>
+        <Instructors></Instructors>
+        <Facilities></Facilities>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
